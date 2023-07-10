774f05c96dcf0312e983cd819384407c624c5d0e
Daniel Taeyoung Sohn <sohn.d.50@gmail.com>
2023-07-10 01:31:54 -0400

    Setup system for encoding and decoding text.
    Implemented flexible and expandible system for making new ciphers. Currently have Caesar cipher implemented.

---
+++ Assets/Scripts/Cipher/BaseCipher.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BaseCipher : MonoBehaviour
+{
+    [Tooltip("The time curve to shift between letters")]
+    [SerializeField] private AnimationCurve letterShiftTimeCurve;
+    [HideInInspector] public CipherSelector.CipherType cipherType = CipherSelector.CipherType.Caesar;
+    public UnityEvent<char> OnCharTranslate;
+
+    public void Encrypt<KeyType>(in string plainText, KeyType key)
+    {
+        StopAllCoroutines();
+        StartCoroutine(EnumerateEncryption(plainText.ToCharArray(), key));
+    }
+
+    protected IEnumerator EnumerateEncryption<KeyType>(char[] translationBuffer, KeyType key)
+    {
+        float time = 0;
+        for (int i = 0; i < translationBuffer.Length; i++)
+        {
+            Encode(ref translationBuffer[i], key);
+            yield return new WaitForSeconds(letterShiftTimeCurve.Evaluate(time));
+            time += Time.fixedDeltaTime;
+        }
+    }
+
+    protected void Encode<KeyType>(ref char character, KeyType key)
+    {
+        switch(cipherType)
+        {
+            case CipherSelector.CipherType.Caesar:
+                switch (key)
+                {
+                    case int shift:
+                        character = (char)(((character + shift - 'a') % 26) + 'a');
+                        break;
+                    default:
+                        Debug.LogError("Incorrect key type");
+                        break;
+                }
+                break;
+            case CipherSelector.CipherType.Vigenere:
+                switch (key)
+                {
+                    case string:
+
+                        break;
+                    default:
+                        Debug.LogError("Incorrect key type");
+                        break;
+                }
+                break;
+            default:
+                break;
+        }
+        OnCharTranslate.Invoke(character);
+    }
+
+    public void Decrypt<KeyType>(in string cipherText, KeyType key)
+    {
+        StopAllCoroutines();
+        StartCoroutine(EnumerateDecryption(cipherText.ToCharArray(), key));
+    }
+
+    protected void Decode<KeyType>(ref char character, KeyType key)
+    {
+        switch (cipherType)
+        {
+            case CipherSelector.CipherType.Caesar:
+                switch (key)
+                {
+                    case int shift:
+                        character = (char)(((character + (26 - shift) - 'a') % 26) + 'a');
+                        break;
+                    default:
+                        Debug.LogError("Incorrect key type");
+                        break;
+                }
+                break;
+            case CipherSelector.CipherType.Vigenere:
+                switch (key)
+                {
+                    case string:
+
+                        break;
+                    default:
+                        Debug.LogError("Incorrect key type");
+                        break;
+                }
+                break;
+            default:
+                break;
+        }
+        OnCharTranslate.Invoke(character);
+    }
+
+    protected IEnumerator EnumerateDecryption<KeyType>(char[] translationBuffer, KeyType key)
+    {
+        float time = 0;
+        for (int i = 0; i < translationBuffer.Length; i++)
+        {
+            Decode(ref translationBuffer[i], key);
+            yield return new WaitForSeconds(letterShiftTimeCurve.Evaluate(time));
+            time += Time.fixedDeltaTime;
+        }
+    }
+}
---
+++ Assets/Scripts/Cipher/CipherSelector.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CipherSelector : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField textInput;
+    [SerializeField] private TMP_InputField keyInput;
+    [SerializeField] private BaseCipher cipher;
+    [SerializeField] private TMP_Dropdown cipherSelect;
+    [SerializeField] private TextMeshProUGUI outputText;
+    public enum CipherType { Caesar, Vigenere }
+    public CipherType cipherType = CipherType.Caesar;
+
+    public void UpdateCipherSelection(int value)
+    {
+        keyInput.text = string.Empty;
+        cipherType = (CipherType)value;
+        cipher.cipherType = cipherType;
+        switch (cipherType)
+        {
+            case CipherType.Caesar:
+                keyInput.characterValidation = TMP_InputField.CharacterValidation.Integer;
+                break;
+            case CipherType.Vigenere:
+                keyInput.characterValidation = TMP_InputField.CharacterValidation.Name;
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void UpdateText(char character)
+    {
+        outputText.text += character;
+    }
+
+    public void EncodeText()
+    {
+        if(!string.IsNullOrEmpty(keyInput.text))
+        {
+            outputText.text = string.Empty;
+            switch (cipherType)
+            {
+                case CipherType.Caesar:
+                    cipher.Encrypt(textInput.text.ToLower(), int.Parse(keyInput.text));
+                    break;
+                case CipherType.Vigenere:
+                    cipher.Encrypt(textInput.text.ToLower(), keyInput.text);
+                    break;
+                default:
+                    break;
+            }
+        }
+        else
+        {
+            EmptyKeyNote();
+        }
+    }
+
+    public void DecodeText()
+    {
+        if (!string.IsNullOrEmpty(keyInput.text))
+        {
+            outputText.text = string.Empty;
+            switch (cipherType)
+            {
+                case CipherType.Caesar:
+                    cipher.Decrypt(textInput.text.ToLower(), int.Parse(keyInput.text));
+                    break;
+                case CipherType.Vigenere:
+                    cipher.Decrypt(textInput.text.ToLower(), keyInput.text);
+                    break;
+                default:
+                    break;
+            }
+        }
+        else
+        {
+            EmptyKeyNote();
+        }
+    }
+
+    private void EmptyKeyNote()
+    {
+        switch (cipherType)
+        {
+            case CipherType.Caesar:
+                outputText.text = "Input a shift value for the key first!";
+                break;
+            case CipherType.Vigenere:
+                outputText.text = "Input a word for the key first!";
+                break;
+            default:
+                break;
+        }
+    }
+}
